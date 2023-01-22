ac44eefdf2ff6ac5eac88fd980a95c6ce926c2ce
AdelinChaushev <106106846+AdelinChaushev@users.noreply.github.com>
2023-01-22 17:16:11 +0200

    Adding First Steps In Coding - Lab

---
+++ First Steps In Coding - Lab/04.Inches to Centimeters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double inch = double.Parse(Console.ReadLine());
+            double sm = inch * 2.54;
+            Console.WriteLine(sm);
+
+        }
+    }
+}
